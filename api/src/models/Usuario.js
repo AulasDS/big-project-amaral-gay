41c6bfc9cfe4e3ec194e1bb8ac03d2a8772c4583
Eduardo Henrique Ribeiro da Silva <eduhrsilva@gmail.com>
2026-06-15 15:00:11 -0300

    Auteracao usuario no api

--- api/src/models/Usuario.js
+++ api/src/models/Usuario.js
@@ -1,9 +1,27 @@
 const mongoose = require('mongoose');
 
-const Usuario = mongoose.model('Usuario', {
-    nome: String,
-    email: String,
-    nascimento: Date,
-})
+const ClienteSchema = new mongoose.Schema(
+    {
+        nome: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        nascimento: {
+            type: Date,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
 
-module.exports = Usuario;
+module.exports = mongoose.model('Cliente', ClienteSchema);
